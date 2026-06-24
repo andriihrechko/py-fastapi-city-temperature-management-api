32be0136afd7119b2ce2b7e288bea56376aefe3e
andriihrechko <andriihrechko250703@gmail.com>
2026-06-25 01:52:28 +0300

    add uvicorn server and fastapi app

---
+++ app/main.py
@@ -0,0 +1,14 @@
+import uvicorn
+from fastapi import FastAPI
+
+from app.routers import city
+
+app = FastAPI(
+    title="Cities API",
+    version="0.1.0",
+    description="API for managing cities",
+    prefix="/api",
+)
+
+
+app.include_router(city.router)
